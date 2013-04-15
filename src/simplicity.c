f4e7943ae29f85c9ea872a83e3a779f516a8130e
Raja Baz <raja@walkingthumbs.com>
2013-04-15 15:44:45 +0300

    Added an AM/PM display. Added some defines to control the placement of stuff on the screen. Moved everything upwards to fit AM/PM display

--- src/simplicity.c
+++ src/simplicity.c
@@ -9,17 +9,21 @@ Window window;
 
 TextLayer text_date_layer;
 TextLayer text_time_layer;
+TextLayer text_am_pm_layer;
 
 Layer line_layer;
 
+#define TEXT_START_Y 48
+#define AM_PM_HIGH_OFFSET 52
+#define AM_PM_LOW_OFFSET 75
 
 void line_layer_update_callback(Layer *me, GContext* ctx) {
     (void)me;
 
     graphics_context_set_stroke_color(ctx, GColorWhite);
 
-    graphics_draw_line(ctx, GPoint(8, 97), GPoint(131, 97));
-    graphics_draw_line(ctx, GPoint(8, 98), GPoint(131, 98));
+    graphics_draw_line(ctx, GPoint(8, TEXT_START_Y+29), GPoint(131, TEXT_START_Y+29));
+    graphics_draw_line(ctx, GPoint(8, TEXT_START_Y+30), GPoint(131, TEXT_START_Y+30));
 
 }
 
@@ -27,7 +31,10 @@ void display_time(PblTm * tick_time) {
     // Need to be static because they're used by the system later.
     static char time_text[] = "00:00";
     static char date_text[] = "Xxxxxxxxx 00";
+    static char am_pm_text[] = "XX";
 
+    static int am_pm_text_is_high = 1; // Insert "high" joke here
+    
     char *time_format;
 
 
@@ -38,9 +45,18 @@ void display_time(PblTm * tick_time) {
 
     if (clock_is_24h_style()) {
         time_format = "%R";
+        memset(am_pm_text, 0, sizeof(am_pm_text));
     } else {
         time_format = "%I:%M";
+        if (tick_time->tm_hour >= 12){
+            am_pm_text[0] = 'P';
+        } else {
+            am_pm_text[0] = 'A';
+        }
+        am_pm_text[1] = 'M';
     }
+
+    text_layer_set_text(&text_am_pm_layer, am_pm_text);
 
     string_format_time(time_text, sizeof(time_text), time_format, tick_time);
 
@@ -48,11 +64,19 @@ void display_time(PblTm * tick_time) {
     // for twelve hour clock.
     if (!clock_is_24h_style() && (time_text[0] == '0')) {
         memmove(time_text, &time_text[1], sizeof(time_text) - 1);
+        if (!am_pm_text_is_high){
+            layer_set_frame(&text_am_pm_layer.layer, GRect(110, TEXT_START_Y+AM_PM_HIGH_OFFSET, 144-110, 168-(TEXT_START_Y+AM_PM_HIGH_OFFSET)));
+            am_pm_text_is_high = 1;
+        }
+    } else {
+        if (am_pm_text_is_high){
+            layer_set_frame(&text_am_pm_layer.layer, GRect(110, TEXT_START_Y+AM_PM_LOW_OFFSET, 144-110, 168-(TEXT_START_Y+AM_PM_LOW_OFFSET)));
+            am_pm_text_is_high = 0;
+        }
     }
 
     text_layer_set_text(&text_time_layer, time_text);    
 }
-
 
 void handle_init(AppContextRef ctx) {
     (void)ctx;
@@ -63,21 +87,29 @@ void handle_init(AppContextRef ctx) {
 
     resource_init_current_app(&APP_RESOURCES);
 
+    GFont small_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_CONDENSED_21));
 
     text_layer_init(&text_date_layer, window.layer.frame);
     text_layer_set_text_color(&text_date_layer, GColorWhite);
     text_layer_set_background_color(&text_date_layer, GColorClear);
-    layer_set_frame(&text_date_layer.layer, GRect(8, 68, 144-8, 168-68));
-    text_layer_set_font(&text_date_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_CONDENSED_21)));
+    layer_set_frame(&text_date_layer.layer, GRect(8, TEXT_START_Y, 144-8, 168-TEXT_START_Y));
+    text_layer_set_font(&text_date_layer, small_font);
     layer_add_child(&window.layer, &text_date_layer.layer);
 
 
     text_layer_init(&text_time_layer, window.layer.frame);
     text_layer_set_text_color(&text_time_layer, GColorWhite);
     text_layer_set_background_color(&text_time_layer, GColorClear);
-    layer_set_frame(&text_time_layer.layer, GRect(7, 92, 144-7, 168-92));
+    layer_set_frame(&text_time_layer.layer, GRect(7, TEXT_START_Y+24, 144-7, 168-(TEXT_START_Y+24)));
     text_layer_set_font(&text_time_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_BOLD_SUBSET_49)));
     layer_add_child(&window.layer, &text_time_layer.layer);
+
+    text_layer_init(&text_am_pm_layer, window.layer.frame);
+    text_layer_set_text_color(&text_am_pm_layer, GColorWhite);
+    text_layer_set_background_color(&text_am_pm_layer, GColorClear);
+    layer_set_frame(&text_am_pm_layer.layer, GRect(110, TEXT_START_Y+AM_PM_HIGH_OFFSET, 144-110, 168-(TEXT_START_Y+AM_PM_HIGH_OFFSET)));
+    text_layer_set_font(&text_am_pm_layer, small_font);
+    layer_add_child(&window.layer, &text_am_pm_layer.layer);
 
 
     layer_init(&line_layer, window.layer.frame);
@@ -91,11 +123,11 @@ void handle_init(AppContextRef ctx) {
     display_time(&tick_time);
 }
 
-
 void handle_minute_tick(AppContextRef ctx, PebbleTickEvent *t) {
     (void)ctx;
     display_time(t->tick_time);
 }
+
 
 void pbl_main(void *params) {
     PebbleAppHandlers handlers = {
