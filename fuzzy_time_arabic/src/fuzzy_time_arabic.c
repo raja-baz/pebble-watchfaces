a70fb8e55f407f14ee60ef6d697fd1577854ddda
Raja Baz <raja@walkingthumbs.com>
2013-04-15 23:41:21 +0300

    Copied-in the source for fuzzy_time from the examples, easier to modify it that to re-write from scratch

--- fuzzy_time_arabic/src/fuzzy_time_arabic.c
+++ fuzzy_time_arabic/src/fuzzy_time_arabic.c
@@ -1,29 +1,64 @@
 #include "pebble_os.h"
 #include "pebble_app.h"
 #include "pebble_fonts.h"
-
+#include "num2words.h"
 
 #define MY_UUID { 0x91, 0xC1, 0xDA, 0xB7, 0x5B, 0xA7, 0x45, 0xE7, 0xA7, 0x68, 0x78, 0xE8, 0x5C, 0xA6, 0x48, 0x16 }
 PBL_APP_INFO(MY_UUID,
-             "Template App", "Your Company",
+             "Arabic Fuzzy Time",
+             "Raja Baz",
              1, 0, /* App version */
              DEFAULT_MENU_ICON,
-             APP_INFO_STANDARD_APP);
+             APP_INFO_WATCH_FACE);
 
-Window window;
+#define BUFFER_SIZE 86
 
+static struct CommonWordsData {
+  TextLayer label;
+  Window window;
+  char buffer[BUFFER_SIZE];
+} s_data;
 
-void handle_init(AppContextRef ctx) {
-  (void)ctx;
+static void update_time(PblTm* t) {
+  fuzzy_time_to_words(t->tm_hour, t->tm_min, s_data.buffer, BUFFER_SIZE);
+  text_layer_set_text(&s_data.label, s_data.buffer);
+}
 
-  window_init(&window, "Window Name");
-  window_stack_push(&window, true /* Animated */);
+static void handle_minute_tick(AppContextRef app_ctx, PebbleTickEvent* e) {
+  update_time(e->tick_time);
+}
+
+static void handle_init(AppContextRef ctx) {
+  (void) ctx;
+
+  window_init(&s_data.window, "My Fuzzy Time");
+  const bool animated = true;
+  window_stack_push(&s_data.window, animated);
+
+  window_set_background_color(&s_data.window, GColorBlack);
+  GFont gotham = fonts_get_system_font(FONT_KEY_DROID_SERIF_28_BOLD);
+
+  text_layer_init(&s_data.label, GRect(0, 20, s_data.window.layer.frame.size.w, s_data.window.layer.frame.size.h - 20));
+  text_layer_set_background_color(&s_data.label, GColorBlack);
+  text_layer_set_text_color(&s_data.label, GColorWhite);
+  text_layer_set_font(&s_data.label, gotham);
+  layer_add_child(&s_data.window.layer, &s_data.label.layer);
+
+  PblTm t;
+  get_time(&t);
+  update_time(&t);
 }
 
 
 void pbl_main(void *params) {
   PebbleAppHandlers handlers = {
-    .init_handler = &handle_init
+    .init_handler = &handle_init,
+
+    .tick_info = {
+      .tick_handler = &handle_minute_tick,
+      .tick_units = MINUTE_UNIT
+    }
+
   };
   app_event_loop(params, &handlers);
 }
